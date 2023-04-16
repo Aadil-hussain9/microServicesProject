043de37e06fb348395a92b3341b3a4a21f7a5066
Aadil <=>
2023-04-16 14:45:02 +0530

    Microservices added

--- address-service/src/main/java/com/infuse/service/AddressService.java
+++ address-service/src/main/java/com/infuse/service/AddressService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class AddressService {
 
     Logger logger = LoggerFactory.getLogger(AddressService.class);
+
     private final AddressRepository addressRepository;
 
     public AddressService(AddressRepository addressRepository) {
